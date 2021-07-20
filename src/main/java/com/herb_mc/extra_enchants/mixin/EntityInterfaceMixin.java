b8a6b20f60cade35f49c3c3b9dc5a74247e4b805
herb-mc <ratpiceveryday@gmail.com>
2021-07-20 13:42:01 -0700

    significant cleanup in projectile mixins, projectiles now have nbt data to store enchantment data

--- src/main/java/com/herb_mc/extra_enchants/mixin/EntityInterfaceMixin.java
+++ src/main/java/com/herb_mc/extra_enchants/mixin/EntityInterfaceMixin.java
@@ -1,48 +1,13 @@
 package com.herb_mc.extra_enchants.mixin;
 
-import it.unimi.dsi.fastutil.objects.Object2DoubleMap;
 import net.minecraft.entity.Entity;
-import net.minecraft.fluid.Fluid;
-import net.minecraft.tag.Tag;
-import net.minecraft.util.math.BlockPos;
-import net.minecraft.util.math.Box;
-import net.minecraft.util.math.Vec3d;
-import net.minecraft.world.World;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.gen.Accessor;
-import org.spongepowered.asm.mixin.gen.Invoker;
+import org.spongepowered.asm.mixin.Shadow;
 
 @Mixin(Entity.class)
-public interface EntityInterfaceMixin {
+public interface EntityMixin {
 
-    @Accessor("world")
-    World getWorld();
-
-    @Accessor("firstUpdate")
-    boolean getFirstUpdate();
-
-    @Accessor("fluidHeight")
-    Object2DoubleMap<Tag<Fluid>> getFluidHeight();
-
-    @Accessor("onGround")
-    void setOnGround(boolean onGround);
-
-    @Invoker("getY")
-    double invokeGetY();
-
-    @Invoker("discard")
-    void invokeDiscard();
-
-    @Invoker("getBoundingBox")
-    Box invokeGetBoundingBox();
-
-    @Invoker("setVelocity")
-    void invokeSetVelocity(Vec3d velocity);
-
-    @Invoker("getVelocity")
-    Vec3d invokeGetVelocity();
-
-    @Invoker("getBlockPos")
-    BlockPos invokeGetBlockPos();
+    @Shadow
+    
 
 }
